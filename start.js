ae7aa4aaf932021c96c971159637017f9567dad4
melinanev <melina.l.nevarez@gmail.com>
2025-03-22 19:46:53 -0600

    continued render fix
    Continuing to try and fix render deployment by creating a yaml file in addition to the start.js addition

--- start.js
+++ start.js
@@ -1,2 +1,8 @@
 // Simple entry point for Render deployment
-require('./server/dist/server.js');
+console.log('Starting application from start.js');
+
+// For ESM/import style modules
+import('./server/dist/server.js').catch(err => {
+  console.error('Error importing server.js:', err);
+  process.exit(1);
+});
